1ac33ad1ad680bd7f622236291c9dc13954e8f6b
Francesca <francescagauci13@gmail.com>
2020-05-16 19:03:27 +0200

    Final

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const port = process.env.PORT || 3000
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/html');
-  res.end('<h1>Hello World</h1>');
+  res.end('<h1>A Node.js Application</h1><h3>A demo application by Francesca Gauci</h3><h5>4ME307VT20 - Internet Architectures</h5>');
 });
 
 server.listen(port,() => {
