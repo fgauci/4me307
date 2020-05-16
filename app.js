d00fe3f5304d3a2a8f963726545f85467514cd72
Francesca <francescagauci13@gmail.com>
2020-05-16 18:55:16 +0200

    Refactored to previous version

--- app.js
+++ app.js
@@ -1,14 +1,12 @@
-const express = require('express')
-const app = express();
+const http = require('http');
+const port = process.env.PORT || 3000
 
-app.get('/', (req, res) => {
-  res.render('index')
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/html');
+  res.end('<h1>Hello World</h1>');
 });
 
-app.listen(8000, () => {
-  console.log('localhost:8000')
+server.listen(port,() => {
+  console.log(`Server running at port `+port);
 });
-
-app.set('view engine', 'pug');
-
-app.use(express.static('public'));
